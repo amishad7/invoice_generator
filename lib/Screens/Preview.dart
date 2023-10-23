a33b189629d8695447279f9c13e15e04bf70aa9b
r32 <neelmaniya81@gmail.com>
2023-10-23 13:59:27 +0530

    Invoice in Process

--- lib/Screens/Preview.dart
+++ lib/Screens/Preview.dart
@@ -21,7 +21,143 @@ class _PDF_ViewState extends State<PDF_View> {
         build: (context) {
           return pw.Column(
             children: [
-              pw.Text("Invoice"),
+              pw.Container(
+                alignment: pw.Alignment.center,
+                //  padding: const pw.EdgeInsets.only(top: 20, left: 30),
+                color: PdfColors.white,
+                height: 70,
+                width: 500,
+                child: pw.Text(
+                  "INVOICE",
+                  style: pw.TextStyle(
+                    fontSize: 30,
+                    fontWeight: pw.FontWeight.bold,
+                    color: PdfColors.black,
+                  ),
+                ),
+              ),
+              pw.Container(
+                height: 500,
+                width: 500,
+                margin: const pw.EdgeInsets.only(),
+                decoration: pw.BoxDecoration(),
+                child: pw.Column(
+                  mainAxisAlignment: pw.MainAxisAlignment.start,
+                  children: [
+                    pw.Divider(),
+                    pw.Row(
+                      mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                      children: [
+                        pw.SizedBox(height: 20),
+                        pw.Text(
+                          "Invoice to: ",
+                          style: pw.TextStyle(
+                            wordSpacing: 1,
+                            fontWeight: pw.FontWeight.normal,
+                            fontSize: 15,
+                          ),
+                        ),
+                        pw.Text(
+                          "TOTAL Due:",
+                          style: pw.TextStyle(
+                            wordSpacing: 1,
+                            fontWeight: pw.FontWeight.normal,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                    pw.SizedBox(height: 30),
+                    pw.Row(
+                      mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                      children: [
+                        pw.Text(
+                          "Archi Radadiya",
+                          style: pw.TextStyle(
+                            wordSpacing: 1,
+                            fontWeight: pw.FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        pw.Text(
+                          "\$ 0.0",
+                          style: pw.TextStyle(
+                            wordSpacing: 1,
+                            fontWeight: pw.FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                    pw.SizedBox(height: 17),
+                    pw.Row(
+                      children: [
+                        pw.Expanded(
+                          flex: 3,
+                          child: pw.Container(
+                            color: PdfColors.black,
+                            child: pw.Text(
+                              "Products ",
+                              style: pw.TextStyle(
+                                wordSpacing: 1,
+                                color: PdfColors.white,
+                                fontWeight: pw.FontWeight.normal,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                        pw.Expanded(
+                          flex: 1,
+                          child: pw.Container(
+                            color: PdfColors.black,
+                            child: pw.Text(
+                              "Qty ",
+                              style: pw.TextStyle(
+                                wordSpacing: 1,
+                                color: PdfColors.white,
+                                fontWeight: pw.FontWeight.normal,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                        pw.Expanded(
+                          flex: 1,
+                          child: pw.Container(
+                            color: PdfColors.black,
+                            child: pw.Text(
+                              "Price ",
+                              style: pw.TextStyle(
+                                wordSpacing: 1,
+                                color: PdfColors.white,
+                                fontWeight: pw.FontWeight.normal,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                        pw.Expanded(
+                          flex: 1,
+                          child: pw.Container(
+                            alignment: pw.Alignment.bottomRight,
+                            color: PdfColors.black,
+                            child: pw.Text(
+                              "Total ",
+                              style: pw.TextStyle(
+                                wordSpacing: 1,
+                                color: PdfColors.white,
+                                fontWeight: pw.FontWeight.normal,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ],
           );
         },
